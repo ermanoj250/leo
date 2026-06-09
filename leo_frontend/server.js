f76ff871c796dd379ad66463f48f57b2aea5c461
Manoj Chaudhary <your_github_email@example.com>
2026-06-09 14:14:41 +0000

    update backend API URL 1

--- leo_frontend/server.js
+++ leo_frontend/server.js
@@ -6,25 +6,27 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+// Home page
 app.get("/", (req, res) => {
     res.render("index");
 });
 
-const axios = require("axios");
-
+// Submit form
 app.post("/submit", async (req, res) => {
-  const response = await axios.post(
-    "http://43.204.19.53:5001/process",
-    req.body
-  );
-  res.send(response.data);
-});
+    try {
+        const response = await axios.post(
+            "http://43.204.19.53:5001/process",
+            req.body
+        );
+
+        res.send(response.data);
     } catch (error) {
         console.error(error.message);
         res.send("Error connecting to backend");
     }
 });
 
+// Start server
 app.listen(3000, () => {
     console.log("Frontend running on port 3000");
 });
